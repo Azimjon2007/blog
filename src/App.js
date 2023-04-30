1ecd4e3f08e7ffded8b7a3390fd9f398e4a0a652
Azimjon2007 <azimtoshtemirov2007@gmail.com>
2023-04-30 19:56:24 +0300

    bu commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+
+function App() {
+  return ( 
+    <>
+
+<section>
+<header>
+<nav>
+<h1 className="bir">
+    BrandName
+</h1 >
+<ul>
+    <li><a href="#">home</a></li>
+    <li><a href="#">product</a></li>
+    <li><a href="#">pricing</a></li>
+    <li><a href="#">contacta</a></li>
+    
+</ul>
+
+<button>Login</button>
+</nav>
+</header>
+</section>
+<div className="pastgi">
+<h3>welcome</h3>
+<h1 className="ikki" >
+  SELLING ON THE <br/>INTERNET LIKE A PRO</h1>
+<p>we know how larde objekts will act. bur</p>   
+</div>
+
+
+  </>
+
+  );
+   }
+
+
+export default App;
